ad3922d39c5b4b57ba097f9204220bf72902fe10
Hicham Jabbour <hicham.jabbour12@gmail.com>
2021-05-02 14:06:53 +0200

    testing spring rest controller

--- src/test/java/com/data/covid19/Covid19ApplicationTests.java
+++ src/test/java/com/data/covid19/Covid19ApplicationTests.java
@@ -1,13 +1,57 @@
 package com.data.covid19;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.Map;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
-@SpringBootTest
+//The @SpringBootTest annotation searches upwards from the test package until it finds a @SpringBootApplication or @SpringBootConfiguration.
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class Covid19ApplicationTests {
+	
+	@LocalServerPort //@Value("${local.server.port})
+	private int port;
+	
+	@Value("${local.management.port}")
+	private int mgt;
+	
+	TestRestTemplate restTemplate = new TestRestTemplate();
 
+	HttpHeaders headers = new HttpHeaders();
+	
+	
 	@Test
-	void contextLoads() {
-	}
+	public void shouldReturn200WhenSendingRequestToControllerTestMethod() throws Exception {
+	    HttpEntity<String> entity = new HttpEntity<String>(null, headers);
+	    
+	    ResponseEntity<String> response = restTemplate.exchange(
+	    		"https://summary-dot-covi19-api.appspot.com/api/test/France",
+				HttpMethod.GET, entity, String.class);
+	    
+	    assertEquals(response.getBody(),"France");
+	  }
+	
+	@Test
+	public void shouldReturn200WhenSendingRequestToControllerGetCountryMethod() throws Exception {
+	    HttpEntity<String> entity = new HttpEntity<String>(null, headers);
+	    
+	    ResponseEntity<String> response = restTemplate.exchange(
+	    		"https://summary-dot-covi19-api.appspot.com/api/summary/France",
+				HttpMethod.GET, entity, String.class);
+	    
+	    assertEquals(response.getStatusCode(),HttpStatus.OK);
+	  }
+
+
 
 }
