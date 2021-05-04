cbb82de615d136ec40ba2d4daca2993656431902
Hicham Jabbour <hicham.jabbour12@gmail.com>
2021-05-04 11:59:20 +0200

    adding a Home pafge that describes the rest service methods

--- src/main/java/com/data/covid19/Covid19Application.java
+++ src/main/java/com/data/covid19/Covid19Application.java
@@ -1,12 +1,28 @@
 package com.data.covid19;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
-public class Covid19Application extends SpringBootServletInitializer{
+public class Covid19Application  extends SpringBootServletInitializer
+implements CommandLineRunner // The CommandLineRunner interface indicates that a bean should run when it is contained within a SpringApplication. It can be used to create command line applications in Spring Boot.
+{
+	  @Autowired
+	  private ApplicationContext applicationContext; // Now we can access the methods of the application context.
+
+
+	  @Override
+	  public void run(String... args) throws Exception {
+		  System.out.println(applicationContext.getDisplayName());
+	      System.out.println(applicationContext.getId());
+	      MyBean myBean = applicationContext.getBean(MyBean.class);
+	      System.out.println(myBean.getApplicationId());
+		
+	  }
 	 
 	  @Override
 	   protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
@@ -16,6 +32,7 @@ public class Covid19Application extends SpringBootServletInitializer{
 	public static void main(String[] args) {
 		SpringApplication.run(Covid19Application.class, args);
 	}
+
 	
 	
 
